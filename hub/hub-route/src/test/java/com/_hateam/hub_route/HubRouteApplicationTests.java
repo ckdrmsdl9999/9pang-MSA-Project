fd88de8024927d29cd3de882c013bf2cebd9aa07
LastCoder_NAM <66324867+20152842@users.noreply.github.com>
2025-03-13 12:26:38 +0900

    Add files via upload

---
+++ hub/hub-route/src/test/java/com/_hateam/hub_route/HubRouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com._hateam.hub_route;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubRouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
