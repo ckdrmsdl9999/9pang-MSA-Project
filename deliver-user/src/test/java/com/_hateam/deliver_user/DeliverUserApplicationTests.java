bab416e270486554f585a204029fd1bcd85e0073
DESKTOP-3KJDFO1\mryoon <ckdrmsdl9999@gmail.com>
2025-03-13 14:55:27 +0900

    initial eureka-server, user,deliver-user

---
+++ deliver-user/src/test/java/com/_hateam/deliver_user/DeliverUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com._hateam.deliver_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
