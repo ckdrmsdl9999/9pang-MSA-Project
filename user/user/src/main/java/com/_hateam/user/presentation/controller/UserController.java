d5c47d9d8e1fd03b902aecedb9e8053f10feeed2
changgeun <ckdrmsdl9999@naver.com>
2025-03-14 11:20:41 +0900

    init common

---
+++ user/user/src/main/java/com/_hateam/user/presentation/controller/UserController.java
@@ -0,0 +1,33 @@
+package com._hateam.user.presentation.controller;
+
+
+import com._hateam.user.domain.model.User;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users/")
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserController {
+
+
+
+    @PostMapping("signup")
+    public void addUser(@RequestBody User user) {
+        System.out.println();
+    //    UserRoles.
+      //  UserRoles
+        //return user;
+    }
+
+
+
+
+
+
+}
