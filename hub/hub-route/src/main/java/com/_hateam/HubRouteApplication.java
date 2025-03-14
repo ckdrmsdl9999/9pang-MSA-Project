16d89a34dfa1078709ef8e13f9717db8581350bd
yng <namgyu967@gmail.com>
2025-03-14 16:20:29 +0900

    init commit

---
+++ hub/hub-route/src/main/java/com/_hateam/HubRouteApplication.java
@@ -0,0 +1,13 @@
+package com._hateam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubRouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HubRouteApplication.class, args);
+	}
+
+}
