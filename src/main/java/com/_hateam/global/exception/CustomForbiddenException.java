e3c623e34f6cdbb9d1774e148f5c038e7da1e1c1
LastCoder_NAM <66324867+20152842@users.noreply.github.com>
2025-03-14 10:30:45 +0900

    Delete src directory

--- src/main/java/com/_hateam/global/exception/CustomForbiddenException.java
+++
@@ -1,8 +0,0 @@
-package com._hateam.global.exception;
-
-public class CustomForbiddenException extends RuntimeException {
-
-	public CustomForbiddenException(String message) {
-		super(message);
-	}
-}
