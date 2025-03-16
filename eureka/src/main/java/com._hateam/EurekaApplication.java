c20392bbd5c47caf5044fed8948b60e821564de1
yng <namgyu967@gmail.com>
2025-03-16 17:30:53 +0900

    - build : EurekaApplication
     - EurekaApplication 관련 어노테이션 추가 및 application.properties 설정

--- eureka/src/main/java/com._hateam/EurekaApplication.java
+++ eureka/src/main/java/com._hateam/EurekaApplication.java
@@ -1,11 +1,12 @@
-package com._hateam.eureka;
+package com._hateam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaApplication.class, args);
 	}
