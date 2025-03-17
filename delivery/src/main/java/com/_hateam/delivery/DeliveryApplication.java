62c7ee86ff3e67870aca6f13e4e2274cdba3fc4b
kss <kos7114@naver.com>
2025-03-18 07:22:16 +0900

    fix: global exception 관련 처리문제
    글로벌 exception을 적용하려 할때 globalexception이 아닌 기본 exception에서 걸리게 되어 원하는 응답을 반환하지 못함
    -> Spring Boot는 기본적으로 @SpringBootApplication 클래스가 있는 패키지와 그 하위 패키지만 스캔하기 때문에 common의 exception까지는 bean으로 등록되지 않아서 생긴 문제
    -> @Import(com.common.exception.GlobalExceptionHandler.class)를 통해 직접적으로 포함하도록 하여 해결

--- delivery/src/main/java/com/_hateam/delivery/DeliveryApplication.java
+++ delivery/src/main/java/com/_hateam/delivery/DeliveryApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -14,7 +16,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaAuditing
 @EnableCaching
 @EnableScheduling
-@EnableFeignClients
+//@EnableFeignClients
+@Import(com._hateam.common.exception.GlobalExceptionHandler.class) // 컴포넌트 스캔 문제로 인한 적용
 public class DeliveryApplication {
 
 	public static void main(String[] args) {
