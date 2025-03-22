cdc79009c9af62d2daf04ccbb332029f37d72add
mango606 <yuj08184@kangwon.ac.kr>
2025-03-22 23:42:09 +0900

    feat: 일일 배송 알림을 위한 DTO 구조 개선
    - 기존 DTO에서 불필요한 필드 제거
    - 배송 위치 및 경로 정보 필드 추가
    - 허브 ID와 배송 담당자 ID 정보 추가
    
    Related to: #61

--- message/src/main/java/com/_hateam/message/application/dto/response/SlackMessageDailyDeliveryResponse.java
+++ message/src/main/java/com/_hateam/message/application/dto/response/SlackMessageDailyDeliveryResponse.java
@@ -5,15 +5,30 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+import java.util.UUID;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class SlackMessageDailyDeliveryResponse {
-    // TODO: 임의로 설정한 필드이므로 수정하기
-    private String batchId;
-    private int staffCount;
-    private int sentMessages;
-    private int failedMessages;
-    private String executionTime;
+    private UUID hubId;                           // 출발 허브 ID
+    private UUID delivererId;                     // 배송 담당자 ID
+    private String delivererName;                 // 배송 담당자 이름
+    private String slackId;                       // 배송 담당자 슬랙 ID
+    private int totalDeliveryPoints;              // 총 배송 지점 수
+    private String sentAt;                        // 알림 발송 시간
+    private List<DeliveryRoutePoint> routePoints; // 방문 지점 정보 리스트
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class DeliveryRoutePoint {
+        private int sequence;                     // 방문 순서
+        private UUID destinationId;               // 목적지 ID (업체 ID 등)
+        private String destinationName;           // 목적지 이름
+        private String destinationAddress;        // 목적지 주소
+    }
 }
